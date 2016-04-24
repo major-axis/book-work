47878cc7c9fef456b51ad223d6db8ff2e6351a51
developer <283426694@qq.com>
2016-04-24 15:46:50 +0800

    modify db address

--- bookhouse/main/config.py
+++ bookhouse/main/config.py
@@ -4,5 +4,5 @@
 DEBUG = True
 SECRET_KEY = 'secret_key'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:root@127.0.0.1:3306/book_house?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:root@127.0.0.1:3306/rest_shop?charset=utf8&use_unicode=0'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
